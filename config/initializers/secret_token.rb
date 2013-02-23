50c0c3d69da72686e0f76d83feb019cef704d1cf
jrcscarrillo <jcarrillo1@madisoncollege.edu>
2013-02-23 13:27:32 -0600

    My first windows based github app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WindowsApp::Application.config.secret_token = 'b5699603f81045ff676a993977b613912e64598dc9da58df1c2249b6a734caf12eea12bdda7b2e3d1103a2e272e3e709798a09bb9ec8cc21ca4496c5783b9737'
